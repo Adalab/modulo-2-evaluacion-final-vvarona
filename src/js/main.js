6cadebb2d017eb4e205c8fffab19d45f1efd079c
vvarona <vvaronad@gmail.com>
2022-07-18 18:36:58 +0200

    add fav list

--- src/js/main.js
+++ src/js/main.js
@@ -8,52 +8,96 @@ const resultList = document.querySelector('.js-result-list');
 const serverURL = 'https://api.jikan.moe/v4/anime?q=';
 const formSearch = document.querySelector('.search');
 
-const animeList = [];
-const animeFavList= [];
+let animeResultList = [];
+const animeFavList = [];
+
+function handleCardClick(event) {
+  const selectedAnimeID = parseInt(event.currentTarget.id);
+  const foundAnime = animeResultList.find((anime)=> anime.mal_id === selectedAnimeID);
+  const favFound = animeFavList.findIndex((favAnime) => favAnime.mal_id === selectedAnimeID);
+  if (favFound === -1) {
+    animeFavList.push(foundAnime);
+  } else {
+    animeFavList.splice(favFound, 1);
+  }
+  console.log('animeFavList:', animeFavList);
+  renderCard(animeResultList, resultList);
+  renderCard(animeFavList, favListHTML);
+ 
+}
 
 function listenCard() {
   const liCard = document.querySelectorAll('.js-anime-card');
   for (const card of liCard) {
-    card.addEventListener('click', ()=> console.log(`click`) );
+    card.addEventListener('click', handleCardClick);
   }
-}
+} 
 
 function getFromServer(userSearch) {
-  fetch(serverURL + userSearch)
+  return fetch(serverURL + userSearch)
     .then((response) => response.json())
-    .then((apiData) => {
-      animeResultList = apiData.data;
-      renderCard(animeResultList);
-      console.log('funciona la api');
-      console.log(animeResultList);
-    }).catch(error => {
+    .then((apiData) => {animeResultList = apiData.data})
+    .catch(error => {
       console.error(error);
     });
 }
 
-function printInResultList(animeTitle, animePhoto) {
-  resultList.innerHTML += `<li class="js-anime-card card">
+/* function printInFavList(animeTitle, animePhoto, animeID, classFav) {
+
+
+  favListHTML.innerHTML += `<li class="js-anime-card card ${classFav}" id="${animeID}">
       <img class="card-image" alt"Imagen de ${animeTitle}" title="Imagen de ${animeTitle}" src="${animePhoto}"/>
       <h3 class="card-title">${animeTitle}</h3>
     </li>`;
   listenCard();
 }
+ */
+/* function printInResultList(animeTitle, animePhoto, animeID, classFav) {
 
-function renderCard(animeDataList) {
-  for (const anime of animeDataList) {
+
+  resultList.innerHTML += `<li class="js-anime-card card ${classFav}" id="${animeID}">
+      <img class="card-image" alt"Imagen de ${animeTitle}" title="Imagen de ${animeTitle}" src="${animePhoto}"/>
+      <h3 class="card-title">${animeTitle}</h3>
+    </li>`;
+  listenCard();
+} */
+
+function renderCard(list, htmlList) {
+  htmlList.innerHTML = '';
+  for (const anime of list) {
     let animeTitle = anime.title;
     let animePhoto = anime.images.jpg.image_url;
-    printInResultList(animeTitle, animePhoto);
+    let animeID = anime.mal_id;
+    let classFav = '';
+    const animeFavIndex = animeFavList.findIndex((favAnime) => favAnime.mal_id === animeID);
+
+    if (animeFavIndex !== -1){
+      classFav = 'fav';
+    } else {
+      classFav = '';
+    }
+
+    if(animePhoto === "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png") {
+      animePhoto = "https://via.placeholder.com/225x350/ff0000/666666/?text=No+image+:(";
+    }
+    
+    htmlList.innerHTML += `<li class="js-anime-card card ${classFav}" id="${animeID}">
+      <img class="card-image" alt"Imagen de ${animeTitle}" title="Imagen de ${animeTitle}" src="${animePhoto}"/>
+      <h3 class="card-title">${animeTitle}</h3>
+    </li>`;
+  listenCard();
     
   }
 }
+
 
 function handleButton(event) {
   event.preventDefault();
   let userSearch = searchInput.value;
   let searchInServer = serverURL + userSearch;
   console.log(searchInServer);
-  getFromServer(searchInServer);
+  getFromServer(searchInServer)
+    .then(()=>{renderCard(animeResultList, resultList)});
 }
 
 function handleInput(event) {
@@ -73,9 +117,6 @@ function handleReset(event) {
   console.log(searchInServer);
 }
 
-
-
-let animeResultList = [];
 
 searchInput.addEventListener('keyup', handleInput);
 resetButton.addEventListener('click', handleReset);
