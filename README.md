484f925f0292aec649d8c865679902f7c401d191
vvarona <vvaronad@gmail.com>
2022-07-19 09:58:42 +0200

    add read me

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+# Anime series sercher
+In this website you can search for you favorite anime series in an extensive database powered by the **Jikan API**, and select the ones you like the most en put them in your favorite list (but you can also, take them out of the favorite list by clicking on them again).
+
+Programed using HTML, CSS and Javascript.
+
+Made with love by Victoria Varona for Adalab Bootcamp module 2, in July 2022.
+
 # modulo-2-evaluacion-final-vvarona
 modulo-2-evaluacion-final-vvarona created by GitHub Classroom
--- src/js/main.js
+++ src/js/main.js
@@ -15,7 +15,8 @@ const placeholderImage = 'https://via.placeholder.com/225x350/3D3D3D/666666/?tex
 let animeResultList = [];
 const animeFavList = [];
 
-function handleCardClick(event) {
+
+function handleFavList(event) {
   const selectedAnimeID = parseInt(event.currentTarget.id);
   const foundAnime = animeResultList.find((anime)=> anime.mal_id === selectedAnimeID);
   const favFound = animeFavList.findIndex((favAnime) => favAnime.mal_id === selectedAnimeID);
@@ -24,7 +25,9 @@ function handleCardClick(event) {
   } else {
     animeFavList.splice(favFound, 1);
   }
-  console.log('animeFavList:', animeFavList);
+}
+function handleCardClick(event) {
+  handleFavList(event);
   renderCard(animeResultList, resultList);
   renderCard(animeFavList, favListHTML);
  
@@ -46,12 +49,8 @@ function getFromServer(userSearch) {
     });
 }
 
-function addFavClass(){
-
-}
-
 function renderCard(list, htmlList) {
-  htmlList.innerHTML = '';
+  emptyList(htmlList);
   for (const anime of list) {
     let animeTitle = anime.title;
     let animePhoto = anime.images.jpg.image_url;
@@ -59,7 +58,7 @@ function renderCard(list, htmlList) {
     let classFav = '';
     const animeFavIndex = animeFavList.findIndex((favAnime) => favAnime.mal_id === animeID);
 
-    
+
     if (animeFavIndex !== -1){
       classFav = 'fav';
     } else {
@@ -69,58 +68,49 @@ function renderCard(list, htmlList) {
     if(animePhoto === serverInvalidImage) {
       animePhoto = placeholderImage;
     }
-    
+
     htmlList.innerHTML += `<li class="js-anime-card card ${classFav}" id="${animeID}">
       <img class="card-image" alt"Imagen de ${animeTitle}" title="Imagen de ${animeTitle}" src="${animePhoto}"/>
       <h3 class="card-title">${animeTitle}</h3>
     </li>`;
-  listenCard();
-    
+
+    listenCard();
+
   }
 }
 
 
 function handleButton(event) {
   event.preventDefault();
-  let userSearch = searchInput.value;
+  const userSearch = searchInput.value;
   let searchInServer = serverURL + userSearch;
-  console.log(searchInServer);
   getFromServer(searchInServer)
-    .then(()=>{renderCard(animeResultList, resultList)});
-}
-
-function handleInput(event) {
-  event.preventDefault();
-  let userSearch = searchInput.value;
-  let searchInServer = serverURL + userSearch;
-  console.log(searchInServer);
-  
-
+    .then(()=>{renderCard(animeResultList, resultList);
+    });
 }
 
 function handleReset(event) {
   event.preventDefault();
-  const reset = searchInput.value = '';
-  let searchInServer = serverURL + reset;
-  resultList.innerHTML = '';
-  console.log(searchInServer);
+  searchInput.value = '';
+  emptyList(resultList);
 }
 
+function emptyList(list){
+  list.innerHTML = '';
+}
 
 function handleFavReset(event) {
   event.preventDefault();
   
-  const allFav = document.querySelectorAll('.fav');
-  for (const anime of allFav) {
-    anime.classList.remove('fav')
+  const allInFavList = document.querySelectorAll('.fav');
+  for (const anime of allInFavList) {
+    anime.classList.remove('fav');
   }
-  
   animeFavList.length = 0;
-  favListHTML.innerHTML = '';
+  emptyList(favListHTML);
   renderCard(animeFavList, favListHTML);
 }
 
-searchInput.addEventListener('keyup', handleInput);
 resetButton.addEventListener('click', handleReset);
 formSearch.addEventListener('click', (event) => event.preventDefault);
 searchButton.addEventListener('click', handleButton);
